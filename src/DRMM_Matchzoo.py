78d6d76e347dcac3ac2aeb5d1388c513d0817d5c
Yannis KARMIM <karmim@poleia.lip6.fr>
2019-06-12 17:57:20 +0200

    drmm matchzoo

--- src/DRMM_Matchzoo.py
+++ src/DRMM_Matchzoo.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matchzoo 
 import gensim
-from bs4 import BeautifulSoup
 import ast
-from os import listdir
+from gensim.models import KeyedVectors
+from bs4 import BeautifulSoup
+from os import listdir,sep
 from os.path import isfile, join
-
-
+from gensim.parsing.preprocessing import preprocess_string,remove_stopwords,strip_numeric, strip_tags, strip_punctuation
+from sklearn.feature_extraction.text import CountVectorizer
 
 def get_all_docs_robust4(folder="/local/karmim/Stage_M1_RI/data/collection"):
     dossier = ['FBIS','FR94','FT','LATIMES']
@@ -14,19 +15,25 @@ def get_all_docs_robust4(folder="/local/karmim/Stage_M1_RI/data/collection"):
 
     for d in dossier : 
         all_doc[d] = [f for f in listdir(folder+'/'+d) if isfile(join(folder+'/'+d, f))]
-
+    return all_doc
 
 
 
 
 class DRMM_Matchzoo:
 
-    def __init__(self,all_doc):
+    def __init__(self,all_doc,CUSTOM_FILTERS = [lambda x: x.lower(),remove_stopwords,strip_numeric, strip_tags, strip_punctuation],embeddings_path="/local/karmim/Stage_M1_RI/data/vocab" ):
         """
+            all_doc : dictionnaire de tout nos documents afin d'itérer dessus. 
 
         """
-        self.d_query = {}
-        self.all_doc= all_doc
+        self.d_query = {} # Notre dictionnaire de query
+        self.all_doc= all_doc # Liste de tout nos documents
+        self.CUSTOM_FILTERS = CUSTOM_FILTERS # Liste de fonction de Préprocessing des docs
+        self.model = KeyedVectors.load_word2vec_format(embeddings_path + sep + "model.bin", binary=True)
+        self.vocabulary = [w for w in self.model.vocab]
+        self.vectorizer = CountVectorizer(analyzer='word', vocabulary=vocabulary, binary=True)
+
 
 
 
@@ -43,9 +50,17 @@ class DRMM_Matchzoo:
 
     
     def load_docs(self,file_doc):
+        self.current_docs={}
         with open(file_doc,"r") as f:
-        soup = BeautifulSoup(f.read(),"xml")
-        #stemmer = SnowballStemmer("french", ignore_stopwords=True)        
+            soup = BeautifulSoup(f.read(),"html.parser")
+        id_ = soup.find_all('docno')
+        text_ = soup.find_all('text')
         
-        return np.array([ d.text for d in soup.find_all("TEXT")])
+        for i in range(len(id_)):
+            self.current_docs[id_[i].text] =  preprocess_string(text_[i].text, self.CUSTOM_FILTERS)[2:]
+
+
+    def embedding_query(self):
+        pass
+    
 
